ac8e74e5420fe6db0afee25be5197ec60ba4f7f1
Abdallah YOUBI IDRISSI <57335762+jancoverr@users.noreply.github.com>
2022-09-27 00:19:20 +0100

    fix all tasks

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -31,25 +31,3 @@ unsigned int _strspn(char *s, char *accept)
 
 }
 
-/* pointer arithmetic version
-unsigned int _strspn(char *s, char *accept)
-{
-	int i = 0, j;
-	int matches = 0;
-	while (*(s + i))
-	{
-		for (j = 0; *(accept + j); j++)
-		{
-			if (*(s + i) == *(accept + j))
-			{
-				matches++;
-				break;
-			}
-			if (*(accept + j + 1) == '\0' && *(s + i) != *(accept + j))
-				return (matches);
-		}
-		i++;
-	}
-	return (matches);
-}
-*/
