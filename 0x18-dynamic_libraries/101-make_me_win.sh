765c60de2d851b10f3d5b225dbe23ef20a93068a
Abdallah YOUBI IDRISSI <57335762+jancoverr@users.noreply.github.com>
2022-12-13 22:27:28 +0100

    add all

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -q -O ../ https://github.com/eadortsu/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libmask.so?raw=true
+export LD_PRELOAD= ../libmask.so
